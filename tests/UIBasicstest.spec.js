579f2697ee27170d0b98eed79cd1756b4dd8a435
monika131001 <monikasavhad78@gmail.com>
2026-03-24 15:26:55 +0530

    First demo test cases

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,12 +1,38 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
+const { TIMEOUT } = require("node:dns");
 
-test('Browser Context Test', async ({browser}) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://swiggy.com");
+test("Browser Context Test", async ({ page }) => {
+  const userName = page.locator("#username");
+  const password = page.locator("#password");
+  const signIn = page.locator("[type='submit']");
+  const cardTitles = page.locator(".card-body a");
+  const errMessage = page.locator("[style*='block']");
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  await userName.fill("Monika");
+  await password.fill("Learning@830$3mK2");
+  await signIn.click();
+  console.log(await errMessage.textContent());
+  await expect(errMessage).toContainText("username");
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await cardTitles.first().textContent());
+  console.log(await cardTitles.nth(1).textContent());
+  const allTitles = await cardTitles.allTextContents(); //May return empty array if elements are not yet loaded in DOM
+  console.log(allTitles);
 });
 
-test ('Page Test', async ({page}) => {
-        await page.goto("https://swiggy.com");
+test.only("UI Controls", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const userName = page.locator("#username");
+  const password = page.locator("#password");
 
-})
+  await userName.fill("Monika");
+  await password.fill("Learning@830$3mK2");
+
+  await page.locator(".radiotextsty").last().click();
+  await page.locator(".form-group select").selectOption("consult");
+  await page.pause();
+});
