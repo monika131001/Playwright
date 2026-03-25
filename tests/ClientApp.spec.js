a42563ab231d198d377bf5b188986acef92e9396
monika131001 <monikasavhad78@gmail.com>
2026-03-25 23:04:08 +0530

    Optimized the code

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,24 +1,61 @@
 const { test, expect } = require("@playwright/test");
 
-test("Login Test", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-  await page.locator(".text-reset").click();
-  await page.locator("#firstName").fill("Monika");
-  await page.locator("#lastName").fill("Avhad");
-  await page.locator("#userEmail").fill("moni323@gmail.com");
-  await page.locator("#userMobile").fill("5857904652");
-  await page.locator(".form-group select").selectOption({ label: "Engineer" });
-  await page.getByLabel("Female").check();
-  await page.locator("#userPassword").fill("Mn@123456");
-  await page.locator("#confirmPassword").fill("Mn@123456");
-  await page.locator("[type='checkbox']").check();
-  await page.locator("[type='submit']").click();
+test("Register Test", async ({ page }) => {
+  const registerBtn = page.locator(".text-reset");
+  const firstName = page.locator("#firstName");
+  const lastName = page.locator("#lastName");
+  const email = page.locator("#userEmail");
+  const mobile = page.locator("#userMobile");
+  const occupationDropdown = page.locator(".form-group select");
+  const password = page.locator("#userPassword");
+  const confirmPassword = page.locator("#confirmPassword");
+  const checkbox = page.locator("[type='checkbox']");
+  const submitBtn = page.locator("[type='submit']");
 
-  await page.locator(".login-wrapper button").click();
-  await page.locator("#userEmail").fill("moni323@gmail.com");
-  await page.locator("#userPassword").fill("Mn@123456");
-  await page.locator("#login").click();
+  const emailId = `moni13@gmail.com`;
+  const pass = "1234567@aA8";
+
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  await registerBtn.click();
+  await firstName.fill("Monika");
+  await lastName.fill("Avhad");
+  await email.fill(emailId);
+  await mobile.fill("5857904652");
+  await occupationDropdown.selectOption({ label: "Engineer" });
+  await page.getByLabel("Female").check();
+  await password.fill(pass);
+  await confirmPassword.fill(pass);
+  await checkbox.check();
+  await submitBtn.click();
+});
+
+test.only("Login Test", async ({ page }) => {
+  const emailLocator = page.locator("#userEmail");
+  const passwordLocator = page.locator("#userPassword");
+  const emailId = "moni13@gmail.com";
+  const password = "Mn@123456";
+  const loginBtn = page.locator("#login");
+  const products = page.locator(".card-body");
+  const productName = "ZARA COAT 3";
+
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+  await emailLocator.fill(emailId);
+  await passwordLocator.fill(password);
+  await loginBtn.click();
   await page.locator(".card-body h5 b").first().waitFor();
   const titles = await page.locator(".card-body b").allTextContents();
-  console.log(titles);
+  // console.log(titles);
+
+  const count = await products.count();
+
+  for (let i = 0; i < count; i++) {
+    if ((await products.nth(i).locator("b").textContent()) === productName) {
+      await products.nth(i).locator("text= Add To Cart").click();
+      break;
+    }
+  }
+  await page.locator("[routerlink*='cart']").click();
+  await page.locator("div li").first().waitFor();
+  const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+  expect(bool).toBeTruthy();
 });
