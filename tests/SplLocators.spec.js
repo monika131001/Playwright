18a7ee10c11e703bc79f32f8196c9ce467b27f9d
monika131001 <monikasavhad78@gmail.com>
2026-03-27 23:57:31 +0530

    Using PW Special Locators on another appln

---
+++ tests/SplLocators.spec.js
@@ -0,0 +1,11 @@
+import {test, expect } from "@playwright/test";
+
+test ("Playwright Special Locators", async ({page}) => {
+
+    page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").check();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+    
+
+})
