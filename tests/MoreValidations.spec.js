8d6033b1bdc1d9f8366a3bec89b1a87070a38806
monika131001 <monikasavhad78@gmail.com>
2026-04-12 19:02:04 +0530

    Uncommented and activated previously written code

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -2,9 +2,9 @@ const { test, expect } = require("@playwright/test");
 
 test.only("Popup validations", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-  // await page.goto("https://google.com");
-  // await page.goBack();
-  // await page.goForward();
+  await page.goto("https://google.com");
+  await page.goBack();
+  await page.goForward();
 
   await expect(page.locator("#displayed-text")).toBeVisible();
   await page.locator("#hide-textbox").click();
