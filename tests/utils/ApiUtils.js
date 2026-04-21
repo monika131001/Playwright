e5c82231aadf85f865c374c04ad02ff7a35896ca
monika131001 <monikasavhad78@gmail.com>
2026-04-21 23:13:41 +0530

    Changed folder structure for util files

---
+++ tests/utils/ApiUtils.js
@@ -0,0 +1,37 @@
+class ApiUtils {
+  constructor(apiContext, loginPayload) {
+    this.apiContext = apiContext;
+    this.loginPayload = loginPayload;
+  }
+  async getToken() {
+    const loginResponse = await this.apiContext.post(
+      "https://rahulshettyacademy.com/api/ecom/auth/login", // Send POST request to login API with user credentials
+      { data: this.loginPayload },
+    );
+    const loginResponseJSON = await loginResponse.json();
+    const token = loginResponseJSON.token;
+    console.log(token);
+    return token;
+  }
+
+  async createOrder(orderPayload) {
+    const response = {};
+    response.token = await this.getToken();
+    const orderResponse = await this.apiContext.post(
+      "https://rahulshettyacademy.com/api/ecom/order/create-order",
+      {
+        data: orderPayload,
+        headers: {
+          Authorization: response.token,
+          "Content-type": "application/json",
+        },
+      },
+    );
+    const orderResponseJSON = await orderResponse.json();
+    console.log(orderResponseJSON);
+    const orderId = orderResponseJSON.orders[0];
+    return orderId;
+  }
+}
+
+module.exports = { ApiUtils };
