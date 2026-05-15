9901b21457da27b9685bc73b6bdb3bed1430b6c3
monika131001 <monikasavhad78@gmail.com>
2026-05-15 23:28:07 +0530

    Upgraded Client App tests with POM

---
+++ PageObjects/PageObjectsManager.js
@@ -0,0 +1,20 @@
+const LoginPage = require("./LoginPage");
+const DashboardPage = require("./DashboardPage");
+
+class PageObjectsManager {
+    constructor(page) {
+        this.page = page;
+        this.loginPage = new LoginPage(this.page);
+        this.dashboardPage = new DashboardPage(this.page);
+    }
+
+    getLoginPage() {
+        return this.loginPage;
+    }
+
+    getDashboardPage() {
+        return this.dashboardPage;
+    }
+}
+
+module.exports = PageObjectsManager;
