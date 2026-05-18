5109251bd4e366454ebe2292e45cc804716eea3e
monika131001 <monikasavhad78@gmail.com>
2026-05-18 22:54:19 +0530

    Page object patterns and Data driven parameterization

---
+++ utils/test-base.js
@@ -0,0 +1,11 @@
+const base = require("@playwright/test");
+
+exports.customtest = base.test.extend(
+    {
+        testDataForOrder: {
+            username: "moni13@gmail.com",
+            password: "Mn@123456",
+            productName: "ZARA COAT 4"
+        }
+    },
+)
